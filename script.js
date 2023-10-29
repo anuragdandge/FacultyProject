b9748d31727d815928ab0ed68c13241b978d07b8
anuragdandge <41123540+anuragdandge@users.noreply.github.com>
2023-10-29 16:56:21 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,31 @@
+const navItems = document.querySelectorAll('.nav-item');
+const tabContent = document.querySelectorAll('.tab');
+let selectedTab = null;
+
+navItems.forEach(item => {
+    item.addEventListener('click', () => {
+        const tabId = item.getAttribute('data-tab');
+
+        // Reset the background color and text color of the previously selected tab
+        if (selectedTab) {
+            selectedTab.style.backgroundColor = '';
+            selectedTab.style.color = '';
+        }
+
+        // Set the background color and text color of the clicked tab
+        item.style.backgroundColor = 'rgb(157, 37, 37)';
+        item.style.color = 'white';
+
+        // Hide all tab content
+        tabContent.forEach(tab => {
+            tab.style.display = 'none';
+        });
+
+        // Show the corresponding tab content
+        document.getElementById(tabId).style.display = 'block';
+
+        // Update the selectedTab variable to the current tab
+        selectedTab = item;
+    });
+});
+
